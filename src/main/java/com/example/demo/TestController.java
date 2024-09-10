c0f38293d85cafaff7d1368364f6a930974ff1c9
Aditi Chavare <aditi.chavare@changehealthcare.com>
2024-09-10 19:15:27 +0530

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/run")
+    public String testMethod(){
+        return "application running";
+    }
+}
